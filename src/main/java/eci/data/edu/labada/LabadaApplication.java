b392790c9212e2690e131590db2c035c7f9e5537
diego <diegofernandoruizrojas@gmail.com>
2023-02-27 14:00:02 -0500

    innitial commit

---
+++ src/main/java/eci/data/edu/labada/LabadaApplication.java
@@ -0,0 +1,13 @@
+package eci.data.edu.labada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eci/data/edu/labada/LabadaApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.data.edu.labada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
